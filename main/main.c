e989644379b93f83e238f136b64298e3829122c6
Rafael <rafaelbrandaobastos@gmail.com>
2025-11-28 19:37:41 -0300

    fix

--- main/main.c
+++ main/main.c
@@ -5,17 +5,14 @@
 #include "esp_log.h"
 
 #define APDS9960_ADDR 0x39
-
 // --------- I2C 0 (Sensor 1) ----------
 #define SDA_0 21
 #define SCL_0 22
 #define PORT_0 I2C_NUM_0
-
 // --------- I2C 1 (Sensor 2) ----------
 #define SDA_1 18
 #define SCL_1 19
 #define PORT_1 I2C_NUM_1
-
 static const char *TAG = "APDS9960";
 
 // Função genérica para escrever
@@ -27,10 +24,8 @@ esp_err_t apds9960_write(i2c_port_t port, uint8_t reg, uint8_t value)
     i2c_master_write_byte(cmd, reg, true);
     i2c_master_write_byte(cmd, value, true);
     i2c_master_stop(cmd);
-
     esp_err_t ret = i2c_master_cmd_begin(port, cmd, pdMS_TO_TICKS(500));
     i2c_cmd_link_delete(cmd);
-
     return ret;
 }
 
@@ -38,16 +33,13 @@ esp_err_t apds9960_write(i2c_port_t port, uint8_t reg, uint8_t value)
 esp_err_t apds9960_read(i2c_port_t port, uint8_t reg, uint8_t *data)
 {
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (APDS9960_ADDR << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd, reg, true);
-
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (APDS9960_ADDR << 1) | I2C_MASTER_READ, true);
     i2c_master_read_byte(cmd, data, I2C_MASTER_LAST_NACK);
     i2c_master_stop(cmd);
-
     esp_err_t ret = i2c_master_cmd_begin(port, cmd, pdMS_TO_TICKS(500));
     i2c_cmd_link_delete(cmd);
 
@@ -57,13 +49,10 @@ esp_err_t apds9960_read(i2c_port_t port, uint8_t reg, uint8_t *data)
 void apds9960_init(i2c_port_t port)
 {
     ESP_LOGI(TAG, "Inicializando APDS9960 no I2C %d...", port);
-
     apds9960_write(port, 0x80, 0x0F); // ENABLE
     vTaskDelay(pdMS_TO_TICKS(10));
-
     apds9960_write(port, 0x8F, 0x02); // Prox gain
     apds9960_write(port, 0x83, 0x20); // LED drive
-
     ESP_LOGI(TAG, "APDS9960 no I2C %d inicializado.", port);
 }
 
@@ -95,11 +84,9 @@ void app_main()
     // Inicializa canais I2C
     i2c_init_channel(PORT_0, SDA_0, SCL_0);
     i2c_init_channel(PORT_1, SDA_1, SCL_1);
-
     // Inicializa ambos sensores
     apds9960_init(PORT_0);
     apds9960_init(PORT_1);
-
     while (1)
     {
         read_proximity(PORT_0, "Sensor 1");
